c219854acd541a2509cf3aca8b9e20c70bac13d0
kai turner <75045999+Da5iid@users.noreply.github.com>
2022-04-02 21:30:08 -0700

    Added py_session_1.py.

---
+++ py_session_1.py
@@ -0,0 +1,9 @@
+#just string things
+#also why does \n put an extra space bar before your string you can't really remove?
+
+name = "Kai R. Turner\n\n"
+pronouns = 'He/They\n\n'
+movie = "While I do not have a be-all end-all favorite movie, I recently enjoyed the freshness that was Dune. The cinematography and world bulding was excellent, and who doesn't love verified hot Timothée Chalamet on screen???\n\n"
+food = "I recently became a ~casual enjoyer~ of blackbrries. Lovely sweet bite-sized bits of juicy fruit good. I'll die by that.\n\n"
+
+print(name, pronouns, movie, food)
